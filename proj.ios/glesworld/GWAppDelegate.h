3ba7ce43f8251fb5b6d3b6aadf7265b644c2cb08
Wu Hao <hao.wu@dreamingame.com>
2013-11-22 13:32:09 +0800

    ios

---
+++ proj.ios/glesworld/GWAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GWAppDelegate.h
+//  glesworld
+//
+//  Created by wuhao on 13-7-9.
+//  Copyright (c) 2013年 wuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ESLViewController;
+
+@interface GWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ESLViewController *viewController;
+
+@end
---
+++ proj.ios/glesworld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  glesworld
+//
+//  Created by wuhao on 13-7-9.
+//  Copyright (c) 2013年 wuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GWAppDelegate class]));
+    }
+}
